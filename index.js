dc403882d9c76076e12d850d3a5b4ccfe5af8656
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-12 00:15:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const drivers = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name)
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver(name) {
+  drivers.pop(name)
+}
+
+function destructivelyRemoveFirstDriver(name) {
+  drivers.shift(name)
+}
+
+function appendDriver(name) {
+  new_array = [...drivers, name]
+  return new_array
+}
+
+function prependDriver(name) {
+  new_array = [name, ...drivers]
+  return new_array
+}
